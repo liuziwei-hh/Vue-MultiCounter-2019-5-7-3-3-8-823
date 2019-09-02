774f082b6f0032cd6a3dcdb1d11f7d5ccb87dc77
LZW <1312299240@qq.com>
2019-09-02 22:09:35 +0800

    sum

---
+++ counter/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <input v-model="nums" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div v-if="parseInt(nums)>0">
+    <Counterv-for="n in parseInt(num)"
+        v-on:update="update"
+        v-bind:key="n"
+        v-bind:index="n"
+        v-bind:counter="sum[n]"/>
+    </div>
+     <p>{{ getSum }}</p>
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Counter
+  },
+  data:function(){
+    return {
+      num: 0,
+      sum: []
+    }
+  },
+  computed: {
+    getSum: function() {
+      let result = 0;
+      for (let item of this.sum) {
+        result += item;
+      }
+      return result;
+    }
+  },
+  methods: {
+    update: function(param) {
+      this.sum[param.index] = param.value;
+    },
+    initSum: function(number) {
+      this.sum = [];
+      for (let i = 0;i <= parseInt(number) ; i++){
+        this.sum.push(i);
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ counter/src/components/Counter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+  <span>{{index}}:</span>
+    <button v-on:click="clickPlus">{{ click1 }}</button>
+        <span>{{number}}</span>
+    <button v-on:click="clickMun">{{ click2 }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  props: {
+    index:Number
+  },
+  data() {
+    return {
+      number: this.index,
+      click1: "+",
+      click2: "-",
+    };
+  },
+  methods: {
+    clickPlus: function() {
+      //this.number++;
+       this.$emit('update',{index:this.index, value:this.counter+1});
+    },
+    clickMun: function() {
+      //this.number--;
+    this.$emit('update',{index:this.index, value:this.counter-1})
+    },
+    update:function(){
+
+    }
+}
+}
+</script>
